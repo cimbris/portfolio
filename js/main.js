c0f302d86c10b0708d3be3862e44693cab814689
Arseniy <arseniy.cimbris@gmail.com>
2023-06-30 16:53:16 +0300

    new feature: mobile-menu

--- js/main.js
+++ js/main.js
@@ -3,4 +3,36 @@ const mobileMenu = document.querySelector('.header__mobile-menu-list');
 
 burger.addEventListener('click', ()=>{
     mobileMenu.classList.toggle('active');
+    burger.classList.toggle('active');
+    document.body.classList.toggle('active')
 })
+
+mobileMenu.querySelectorAll('.header__menu-link').forEach((item)=>{
+    item.addEventListener('click',() => {
+        mobileMenu.classList.remove('active');
+        burger.classList.remove('active');
+        document.body.classList.remove('active')
+    })
+})
+
+// smooth scroll to anchor-link
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        let href = this.getAttribute('href').substring(1);
+
+        const scrollTarget = document.getElementById(href);
+
+        const topOffset = document.querySelector('.header').offsetHeight;
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
+
